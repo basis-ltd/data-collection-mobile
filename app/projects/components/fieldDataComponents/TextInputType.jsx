775da0bd422c0a67cbe44d23b8fd3dd299d0442e
Nishimwe Prince <princeelysee@gmail.com>
2024-03-05 12:45:16 +0200

    Creating validation and field Types input

---
+++ app/projects/components/fieldDataComponents/TextInputType.jsx
@@ -0,0 +1,66 @@
+import { StyleSheet, View } from "react-native";
+import AppInput from "../../../../components/AppInput";
+import { Formik } from "formik";
+import { inputTypes } from "../../../../constants/inputTypes";
+import { inputValidationSchema } from "../../../../utils/validationchemas";
+import { returnKeyBoardtype } from "../../../../utils/returnKeyBoardType"
+import AppTextarea from "../../../../components/AppTextarea";
+
+
+const TextInputType = ({ field }) => {
+
+    const handleSubmit = (values) => {
+        console.log(values, 'test values')
+    };
+
+    const validationSchema = inputValidationSchema(field);
+
+    return (
+        <Formik
+            initialValues={{ value: "" }}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+        >
+            {({ errors, values, handleChange }) => {
+                return (
+                    <View style={styles.formikContainer}>
+                        {!field.field_type === inputTypes.textarea &&
+                            <AppInput
+                                iconUrl={null}
+                                placeholder={field.placeholder}
+                                keyboardType={returnKeyBoardtype(field.field_type)}
+                                error={errors.value}
+                                value={values.value}
+                                onChangeText={handleChange("value")}
+                                labelText={field.label}
+                            />
+                        }
+                        {field.field_type === inputTypes.textarea &&
+                            <AppTextarea
+                                placeholder={field.placeholder}
+                                error={errors.value}
+                                value={values.value}
+                                onChangeText={handleChange("value")}
+                                labelText={field.label}
+                            />
+                        }
+                    </View>
+                );
+            }}
+        </Formik>
+    );
+};
+
+const styles = StyleSheet.create({
+    formikContainer: {
+        padding: 0,
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        margin: 0,
+        flexDirection: "column",
+        gap: 10,
+    },
+});
+
+export default TextInputType;
