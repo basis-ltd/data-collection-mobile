ef1406595afe6ad14cd3d58fbea081ae58a20bd7
Nishimwe Prince <princeelysee@gmail.com>
2024-03-15 18:24:43 +0200

    Save Data working with Location'

--- helpers/groupArrayByKey.js
+++ helpers/groupArrayByKey.js
@@ -1,16 +1,12 @@
 export const groupArrayByKey = (arr = [], key = 'sectionName') => {
 
-    const groupedBySection = arr.reduce((acc, cur) => {
-        // If the section doesn't exist in the accumulator, create it
+    const groupBySection = arr.reduce((acc, cur) => {
         if (!acc[cur[key]]) {
             acc[cur[key]] = [];
         }
-        // Push the current object into the appropriate section
         acc[cur[key]].push(cur);
         return acc;
     }, {});
 
-    // Extract the groups into an array of subarrays
-    const result = Object.values(groupedBySection);
-    return result
+    return Object.values(groupBySection)
 }
